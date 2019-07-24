9baf82abcd24bf1ec9e0380fec13cff60145efdc
Andrew McVeigh <andrew.x.mcveigh@gmail.com>
2019-07-23 22:18:56 -0700

    no message

--- main.js
+++ main.js
@@ -19,6 +19,21 @@ function _openLinkedVSCode(element) {
   // read the first line of the doc and parse into 2 parts
   var doc = element.documentation;
   console.log(doc);
+  var first = doc.split("\n")[0];
+  if (!first.startsWith("->")) {
+    app.toast.warning(
+      "No VSCode navigation instruction! Form is ->file:pattern"
+    );
+    return;
+  }
+  var parts = first.substring(2).split(":");
+  var file = parts[0];
+  var pattern = parts[1];
+  if (!file) {
+    app.toast.warning("No file specified in navigation instruction");
+    return;
+  }
+  console.log("File = " + file + ", pattern = " + pattern);
 }
 
 /** Initialize Extension */
