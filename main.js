4952c6700a06e3f164c1d0a1c0a9134fb527847a
Andrew McVeigh <andrew.x.mcveigh@gmail.com>
2019-08-08 01:24:28 -0700

    added both set and get location commands

--- main.js
+++ main.js
@@ -2,20 +2,10 @@
 /*global define, $, app, window */
 
 function _handleOpenLinkedVSCode() {
-  var selectedModels = app.selections.getSelectedModels();
-
-  if (selectedModels.length == 0) {
-    app.toast.warning("No items selected.");
-    return;
-  } else if (selectedModels.length > 1) {
-    app.toast.warning("Select only one item.");
+  var element = getSelectedElement();
+  if (!element) {
     return;
   }
-  var element = app.selections.getSelected();
-  _openLinkedVSCode(element);
-}
-
-function _openLinkedVSCode(element) {
   // read the first line of the doc and parse into 2 parts
   var doc = element.documentation;
   console.log(doc);
@@ -33,29 +23,54 @@ function _openLinkedVSCode(element) {
     app.toast.warning("No file specified in navigation instruction");
     return;
   }
-  console.log(
-    $.ajax({
-      type: "POST",
-      url: "http://127.0.0.1:1781/api/commands/vscode.open",
-      beforeSend: function(xhr) {
-        xhr.setRequestHeader(
-          "Authorization",
-          "Basic " + btoa("amcveigh:tiggle")
-        );
-      },
-      data: '"src/genbase.ts"',
-      success: function(data) {
-        console.log(data);
-      },
-      dataType: "json"
-    })
-  );
+  $.ajax({
+    type: "POST",
+    url: "http://127.0.0.1:8081/setLocation",
+    data: JSON.stringify({
+      file: file,
+      pattern: pattern
+    }),
+    success: function(data) {
+      console.log(data);
+    },
+    dataType: "json"
+  });
+}
+
+function _handleGetLinkedVSCode() {
+  var element = getSelectedElement();
+  if (!element) {
+    return;
+  }
+  $.ajax({
+    type: "GET",
+    url: "http://127.0.0.1:8081/getLocation",
+    success: function(data) {
+      element.documentation = "->" + data.file + ":" + data.pattern;
+    },
+    dataType: "json"
+  });
+}
+
+function getSelectedElement() {
+  var selectedModels = app.selections.getSelectedModels();
+
+  if (selectedModels.length == 0) {
+    app.toast.warning("No items selected.");
+    return null;
+  } else if (selectedModels.length > 1) {
+    app.toast.warning("Select only one item.");
+    return null;
+  }
+  return app.selections.getSelected();
 }
 
 /** Initialize Extension */
 function init() {
   var CMD_OPEN_LINKED_VSCODE = "linked_vscode_navigator:open_linked_vscode";
   app.commands.register(CMD_OPEN_LINKED_VSCODE, _handleOpenLinkedVSCode);
+  var CMD_GET_LINKED_VSCODE = "linked_vscode_navigator:get_linked_vscode";
+  app.commands.register(CMD_GET_LINKED_VSCODE, _handleGetLinkedVSCode);
 }
 
 exports.init = init;
